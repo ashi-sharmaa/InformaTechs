ddb495e24b90295e52cecea67ec8dc40ea9ac075
ashi-sharmaa <ashisharma@utexas.edu>
2024-11-26 14:46:51 -0600

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,12 @@
+// const jobs = ["Software Engineer", "Web Developer", "UX Designer", "Mobile App Developer", "IT Project Manager", "Info Security Analyst", "Cybersecurity Business Manager", "Systems Architect", "AI Engineer", "Computer Hardware Engineer", "Video Game Developer", "Product Sales Manager", "Animator", "Data Analyst"];
+
+// function randomJob() {
+//   var rand = (int) (Math.random() * jobs.length); 
+//   $( ".jobs" ).append(jobs[rand]  );
+//   "<p>" + jobs[0] + "</p>"
+// }
+
+// .jobs{
+
+// }
+
